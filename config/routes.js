a85a19f6f92d7074303258916b8c988f7d54f229
BryceEvans <evans.bryce@gmail.com>
2019-01-26 12:03:44 -0700

    Figuring it out. Getting something, but not the response I want yet...

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,32 @@
 const axios = require('axios');
+// define a database
+const db = require('../database/dbConfig');
+// define bcrypt
+const bcrypt = require('bcryptjs');
+// define jwt
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
+
+// secret in .env file
+const secret = 
+  process.env.JWT_SECRET ||
+
+
+// generate token
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1hr',
+    jwtid: '12345' // jti --> like Luis used in auth-ii lecture
+  }
+  //return token
+  return jwt.sign(payload, secret, options);
+  
+};
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,6 +36,26 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.username, 10);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+
+      db('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token})
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        })
+    })
 }
 
 function login(req, res) {
